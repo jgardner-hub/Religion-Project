10ca644d963adbd4b751012df2ef1ed3476a6bcf
jgardner-hub <90541201+jgardner-hub@users.noreply.github.com>
2022-04-09 23:55:52 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,4 +6,24 @@ $(window).scroll(function() {
       $('.navbar').css('background', 'transparent');
       $('.navbar').css('height', '12vh');
     }
-  });
+});
+
+
+// var mybutton = document.getElementById("totop");
+
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () { scrollFunction() };
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//         mybutton.css.display = "block";
+//     } else {
+//         mybutton.css.display = "none";
+//     }
+// }
+
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// }
